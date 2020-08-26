38af5d29f8364db01377d213ce486f0868d6fd74
vaishnavipardeshi22@gmail.com <vaishnavipardeshi22@gmail.com>
2020-08-26 23:36:21 +0530

    [Vaishnavi] ADDED : Code to calculate part time daily wage of employee

--- EmployeeWageComputations/app.js
+++ EmployeeWageComputations/app.js
@@ -1,12 +1,15 @@
 console.log("****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION ******************************");
 // CONSTANTS
-var IS_PRESENT = 1;
+var IS_PART_TIME = 1;
+var IS_FULL_TIME = 2;
 var EMP_WAGE_RATE_PER_HOUR = 20;
 var employeeHrs;
 // GENERATE RANDOM VALUE
-var randomCheck = Math.round(Math.random());
+var randomCheck = Math.round(Math.random() * 10) % 3;
 // CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
-if (randomCheck == IS_PRESENT)
+if (randomCheck == IS_PART_TIME)
+    employeeHrs = 4;
+else if (randomCheck == IS_FULL_TIME)
     employeeHrs = 8;
 else
     employeeHrs = 0;
--- EmployeeWageComputations/app.ts
+++ EmployeeWageComputations/app.ts
@@ -1,18 +1,21 @@
 console.log("****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION ******************************");
 
 // CONSTANTS
-const IS_PRESENT = 1
-const EMP_WAGE_RATE_PER_HOUR = 20
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const EMP_WAGE_RATE_PER_HOUR = 20;
 
 var employeeHrs;
 
 // GENERATE RANDOM VALUE
-let randomCheck = Math.round(Math.random());
+let randomCheck = Math.round(Math.random() * 10) % 3;
 
 // CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
-if (randomCheck == IS_PRESENT)
-    employeeHrs = 8
-else employeeHrs = 0
+if (randomCheck == IS_PART_TIME)
+    employeeHrs = 4;
+else if (randomCheck == IS_FULL_TIME)
+    employeeHrs = 8;
+else employeeHrs = 0;
 
 // CALCULATE DAILY WAGE FOR EMPLOYEE
 var salary = (employeeHrs * EMP_WAGE_RATE_PER_HOUR);
