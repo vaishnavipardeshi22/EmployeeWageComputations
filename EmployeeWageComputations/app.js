d3512025ffa34579c67ae48a058c05e178f8a074
vaishnavipardeshi22@gmail.com <vaishnavipardeshi22@gmail.com>
2020-08-26 23:00:02 +0530

    [Vaishnavi] ADDED : Code to check employee is present or absent

--- EmployeeWageComputations/app.js
+++ EmployeeWageComputations/app.js
@@ -1,1 +1,11 @@
 console.log('****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION ******************************');
+// CONSTANT
+var IS_PRESENT = 1;
+// GENERATE RANDOM VALUE
+var randomCheck = Math.round(Math.random());
+console.log(randomCheck);
+// CHECK EMPLOYEE IS PRESENT OR ABSENT AND PRINT RESULT
+if (randomCheck == IS_PRESENT)
+    console.log("Employee is present.");
+else
+    console.log("Employee is absent.");
--- EmployeeWageComputations/app.ts
+++ EmployeeWageComputations/app.ts
@@ -1,1 +1,15 @@
 console.log('****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION ******************************');
+
+// CONSTANT
+
+const IS_PRESENT = 1
+
+// GENERATE RANDOM VALUE
+
+let randomCheck = Math.round(Math.random());
+
+// CHECK EMPLOYEE IS PRESENT OR ABSENT AND PRINT RESULT
+
+if (randomCheck == IS_PRESENT)
+    console.log("Employee is Present.");
+else console.log("Employee is Absent.");
