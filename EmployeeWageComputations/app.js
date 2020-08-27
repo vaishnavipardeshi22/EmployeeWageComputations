2c5cc1f163b91edbf3865c4f38ee602e170d62e1
vaishnavipardeshi22@gmail.com <vaishnavipardeshi22@gmail.com>
2020-08-27 10:17:28 +0530

    [Vaishnavi] ADDED : Code to calculate monthly wage of employee

--- EmployeeWageComputations/app.js
+++ EmployeeWageComputations/app.js
@@ -1,24 +1,29 @@
 console.log("****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION ******************************");
 // CONSTANTS
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const EMP_WAGE_RATE_PER_HOUR = 20;
+var IS_PART_TIME = 1;
+var IS_FULL_TIME = 2;
+var EMP_WAGE_RATE_PER_HOUR = 20;
+var NUM_WORKING_DAYS = 20;
 var employeeHrs;
-// GENERATE RANDOM VALUE
-let randomCheck = Math.round(Math.random() * 10) % 3;
-// CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
-switch (randomCheck) {
-    case IS_FULL_TIME:
-        employeeHrs = 8;
-        break;
-    case IS_PART_TIME:
-        employeeHrs = 4;
-        break;
-    default:
-        employeeHrs = 0;
-        break;
+var totalSalary = 0;
+// FOR LOOP TO GET MONTHLY WAGE OF EMPLOYEE
+for (var days = 1; days <= NUM_WORKING_DAYS; days++) {
+    // GENERATE RANDOM VALUE
+    var randomCheck = Math.round(Math.random() * 10) % 3;
+    // CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
+    switch (randomCheck) {
+        case IS_FULL_TIME:
+            employeeHrs = 8;
+            break;
+        case IS_PART_TIME:
+            employeeHrs = 4;
+            break;
+        default:
+            employeeHrs = 0;
+            break;
+    }
+    // CALCULATE TOTAL SALRY FOR EMPLOYEE
+    var salary = (employeeHrs * EMP_WAGE_RATE_PER_HOUR);
+    totalSalary += salary;
 }
-// CALCULATE DAILY WAGE FOR EMPLOYEE
-var salary = (employeeHrs * EMP_WAGE_RATE_PER_HOUR);
-console.log("Daily Employee Wage for " + employeeHrs + " Hours : " + salary);
-//# sourceMappingURL=app.js.map
+console.log("Daily Employee Wage for " + employeeHrs + " Hours : " + totalSalary);
--- EmployeeWageComputations/app.ts
+++ EmployeeWageComputations/app.ts
@@ -4,25 +4,32 @@ console.log("****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const EMP_WAGE_RATE_PER_HOUR = 20;
+const NUM_WORKING_DAYS = 20;
 
 var employeeHrs;
+var totalSalary = 0;
 
-// GENERATE RANDOM VALUE
-let randomCheck = Math.round(Math.random() * 10) % 3;
+// FOR LOOP TO GET MONTHLY WAGE OF EMPLOYEE
+for (let days = 1; days <= NUM_WORKING_DAYS; days++) {
+    // GENERATE RANDOM VALUE
+    let randomCheck = Math.round(Math.random() * 10) % 3;
 
-// CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
-switch (randomCheck) {
-    case IS_FULL_TIME:
-        employeeHrs = 8;
-        break;
-    case IS_PART_TIME:
-        employeeHrs = 4;
-        break;
-    default:
-        employeeHrs = 0;
-        break;
+    // CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
+    switch (randomCheck) {
+        case IS_FULL_TIME:
+            employeeHrs = 8;
+            break;
+        case IS_PART_TIME:
+            employeeHrs = 4;
+            break;
+        default:
+            employeeHrs = 0;
+            break;
+    }
+
+    // CALCULATE TOTAL SALRY FOR EMPLOYEE
+    var salary = (employeeHrs * EMP_WAGE_RATE_PER_HOUR);
+    totalSalary += salary;
 }
 
-// CALCULATE DAILY WAGE FOR EMPLOYEE
-var salary = (employeeHrs * EMP_WAGE_RATE_PER_HOUR);
-console.log("Daily Employee Wage for " + employeeHrs + " Hours : " + salary);
+console.log("Daily Employee Wage for " + employeeHrs + " Hours : " + totalSalary);
