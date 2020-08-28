0bb7f8694e48f5c296377fa5bd1c0778de35093a
vaishnavipardeshi22@gmail.com <vaishnavipardeshi22@gmail.com>
2020-08-28 07:58:45 +0530

    [Vaishnavi] REFACTORED : Code to calculate working hours using function

--- EmployeeWageComputations/app.js
+++ EmployeeWageComputations/app.js
@@ -1,20 +1,17 @@
 console.log("****************************** WELCOME TO EMPLOYEE WAGE COMPUTATION ******************************");
 // CONSTANTS
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const EMP_WAGE_RATE_PER_HOUR = 20;
-const NUM_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 100;
+var IS_PART_TIME = 1;
+var IS_FULL_TIME = 2;
+var EMP_WAGE_RATE_PER_HOUR = 20;
+var NUM_WORKING_DAYS = 20;
+var MAX_HRS_IN_MONTH = 100;
 var employeeHrs;
 var totalSalary = 0;
 var totalWorkingDays = 0;
 var totalEmpHrs = 0;
-// WHILE LOOP TO GET MONTHLY WAGE OF EMPLOYEE
-while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
-    totalWorkingDays++;
-    // GENERATE RANDOM VALUE
-    let randomCheck = Math.round(Math.random() * 10) % 3;
-    // CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
+var employeeCheck;
+// FUNCTION TO GET WORKING HOURS
+var getWorkingHours = function (randomCheck) {
     switch (randomCheck) {
         case IS_FULL_TIME:
             employeeHrs = 8;
@@ -26,10 +23,14 @@ while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
             employeeHrs = 0;
             break;
     }
+};
+// WHILE LOOP TO GET MONTHLY WAGE OF EMPLOYEE
+while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
+    totalWorkingDays++;
     // CALCULATE TOTAL EMPLOYEE HOURS
+    employeeCheck = getWorkingHours(Math.round(Math.random() * 10) % 3);
     totalEmpHrs += employeeHrs;
 }
 // CALCULATE TOTAL SALARY
 totalSalary = (totalEmpHrs * EMP_WAGE_RATE_PER_HOUR);
-console.log("Daily Employee Wage for " + employeeHrs + " Hours : " + totalSalary);
-//# sourceMappingURL=app.js.map
+console.log("Daily Employee Wage for " + totalEmpHrs + " Hours : " + totalSalary);
--- EmployeeWageComputations/app.ts
+++ EmployeeWageComputations/app.ts
@@ -11,34 +11,36 @@ var employeeHrs;
 var totalSalary = 0;
 var totalWorkingDays = 0;
 var totalEmpHrs = 0;
+let employeeCheck;
+
+// FUNCTION TO GET WORKING HOURS
+let getWorkingHours = (randomCheck: number) => {
+
+	switch (randomCheck) {
+		case IS_FULL_TIME:
+			employeeHrs = 8;
+			break;
+
+		case IS_PART_TIME:
+			employeeHrs = 4;
+			break;
+
+		default:
+			employeeHrs = 0;
+			break;
+	}
+}
 
 // WHILE LOOP TO GET MONTHLY WAGE OF EMPLOYEE
 while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
 
-    totalWorkingDays++;
+	totalWorkingDays++;
 
-    // GENERATE RANDOM VALUE
-    let randomCheck = Math.round(Math.random() * 10) % 3;
-
-    // CHECK EMPLOYEE IS PRESENT OR ABSENT AND GET EMPLOYEE HOURS
-    switch (randomCheck) {
-        case IS_FULL_TIME:
-            employeeHrs = 8;
-            break;
-
-        case IS_PART_TIME:
-            employeeHrs = 4;
-            break;
-
-        default:
-            employeeHrs = 0;
-            break;
-    }
-
-    // CALCULATE TOTAL EMPLOYEE HOURS
-     totalEmpHrs += employeeHrs;
+	// CALCULATE TOTAL EMPLOYEE HOURS
+	employeeCheck = getWorkingHours(Math.round(Math.random() * 10) % 3);
+	totalEmpHrs += employeeHrs;
 }
 
 // CALCULATE TOTAL SALARY
 totalSalary = (totalEmpHrs * EMP_WAGE_RATE_PER_HOUR);
-console.log("Daily Employee Wage for " + employeeHrs + " Hours : " + totalSalary);
+console.log("Monthly Employee Wage for " + totalEmpHrs + " Hours : " + totalSalary);
